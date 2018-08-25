e6a12fd3dc72174066c28a3eec875095bd0e3abd
DESKTOP-SLA46FM\KAZUHISA NAKAMURA <kaz.nakamura.193@gmail.com>
2018-08-25 23:20:04 +0900

    migration and make admin

--- hello/views.py
+++ hello/views.py
@@ -16,8 +16,17 @@ class HelloView(TemplateView):
        return render(request, 'hello/index.html', self.params)
    
    def post(self, request):
+<<<<<<< HEAD
        ch = request.POST.getlist('choice')
-       self.params['result']= 'selected: '+ str(ch) +'.'
+       result='<ol><b>selected:</b>'
+       for item in ch:
+           result+='<li>' + item + '</li>'
+       result+='</ol>'
+       self.params['result']= result
+=======
+       ch = request.POST['choice']
+       self.params['result']= 'selected: "'+ ch +'".'
+>>>>>>> parent of 395f48d... multichoice
        self.params['form']= HelloForm(request.POST)
        return render(request,'hello/index.html',self.params)
 
